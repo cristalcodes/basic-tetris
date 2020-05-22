8007aa75c949310be1d60fc062698a1e1bb7a2c2
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-05-21 22:07:07 -0700

    Adds draw, undraw, moveDown, and freeze functions.

--- app.js
+++ app.js
@@ -15,32 +15,82 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
   const zTetromino = [
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1],
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1]
+    [0,width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0,width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
   ]
 
   const tTetromino = [
-    [1,width,width+1,width+2],
-    [1,width+1,width+2,width*2+1],
-    [width,width+1,width+2,width*2+1],
-    [1,width,width+1,width*2+1]
+    [1,width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
   ]
 
   const oTetromino = [
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1]
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
   ]
 
   const iTetromino = [
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3],
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3]
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
   ]
 
+const theTetrominoes =[lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+
+let currentPosition = 4
+let currentRotation = 0
+
+//randomly select a tetromino and its first rotation
+let random = Math.floor(Math.random()*theTetrominoes.length)
+console.log(random)
+
+
+let current = theTetrominoes[random][0]
+
+//draw the tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+
+//undraw the tetromino
+  function undraw () {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+
+  // make the tetromino move down every second
+
+  timerId = setInterval(moveDown, 1000)
+
+  // move down function
+  function moveDown () {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+// freeze statement
+function freeze() {
+  if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+    current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+    // start a new tetromino falling
+    random = Math.floor(Math.random() * theTetrominoes.length)
+    current = theTetrominoes[random][currentRotation]
+    currentPosition = 4
+    draw()
+  }
+}
 
 })
